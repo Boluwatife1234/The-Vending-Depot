ba0df17ad50aed9151414da63f04088762019f16
Boluwatife1234 <33322870+Boluwatife1234@users.noreply.github.com>
2022-12-05 16:31:08 +0100

    Add files via upload

---
+++ src/TVD/success.js
@@ -0,0 +1,14 @@
+function changeTextOrder()
+{
+    document.getElementById("orderSuccessful").innerHTML = "Order Successful!"
+}
+setTimeout("changeTextOrder()", 1000);
+
+function changeTextDelievery()
+{
+    document.getElementById("estimatedDelivery").innerHTML = "Delivery will take between 3 - 5 working days"
+}
+setTimeout("changeTextDelievery()", 1000);
+
+
+
